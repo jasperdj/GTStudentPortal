95a4f643633a0d36263aed9b03a641e6a65053f7
JasperDeJong <jasper.dejong@getthere.nl>
2016-09-26 15:19:04 +0200

    initial commit

---
+++ src/main/java/nl/getthere/GtStudentPortalApplication.java
@@ -0,0 +1,12 @@
+package nl.getthere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtStudentPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtStudentPortalApplication.class, args);
+	}
+}
