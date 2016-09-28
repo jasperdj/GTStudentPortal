e6e5cdb0e22e3cf88825c7674b97f3fd735cdc39
Ruud Zonnenberg <ruud.zonnenberg@getthere.nl>
2016-09-28 10:34:37 +0200

    Removed unused imports.

--- src/main/java/nl/getthere/controllers/UserController.java
+++ src/main/java/nl/getthere/controllers/UserController.java
@@ -1,12 +1,9 @@
 package nl.getthere.controllers;
 
-import nl.getthere.model.User;
 import nl.getthere.model.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
