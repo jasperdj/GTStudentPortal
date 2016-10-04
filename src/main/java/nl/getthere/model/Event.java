71c0279b4fcf706a3033e9784a569549981442bc
jasperdj <jjasperdejong@gmail.com>
2016-10-04 11:31:38 +0200

    Added docs

---
+++ src/main/java/nl/getthere/model/Event.java
@@ -0,0 +1,10 @@
+package nl.getthere.model;
+
+/**
+ * Created by jasper.dejong on 4-10-2016.
+ */
+public class Event {
+    private List<EventType> eventTypes;
+    private List<EventTheme> eventThemes;
+
+}
