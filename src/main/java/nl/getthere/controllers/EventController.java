46875d49f488ca618001eee09afe66b8f063d531
Ruud Zonnenberg <ruud.zonnenberg@getthere.nl>
2016-10-07 10:37:08 +0200

    Added eventdetail mapping and permission for all.

--- src/main/java/nl/getthere/controllers/EventController.java
+++ src/main/java/nl/getthere/controllers/EventController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -46,6 +47,13 @@ public class EventController {
         return new EventTheme();
     }
 
+    @RequestMapping("/events/{eventid}")
+    public String showEventDetail(@PathVariable Long eventid, Model model){
+    	Event e = eventRepo.findOne(eventid);
+    	model.addAttribute("event", e);
+    	return "eventdetail";
+    }
+    
     @RequestMapping(value = "/api/newEvent", method = RequestMethod.POST)
     public String newEventPost(Model model, @Valid Event event, BindingResult result) {
         model.addAttribute("error", result);
--- src/main/java/nl/getthere/security/SecurityConfig.java
+++ src/main/java/nl/getthere/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login","/", "/logout", "/registration*", "/registration/event/*", "/api/**").permitAll()
+                .antMatchers("/login","/", "/logout", "/registration*", "/registration/event/*", "/events/*", "/api/**").permitAll()
                 .antMatchers("/resources*").authenticated()
                 .antMatchers("/students*", "/students").hasAuthority("recruiter")
                 .antMatchers("/student*", "/student").hasAuthority("recruiter")
