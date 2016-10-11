d39f5dbd3452879e6c8c27ef4a599476ee16a910
Ruud Zonnenberg <ruud.zonnenberg@getthere.nl>
2016-10-11 14:54:44 +0200

    Email messages are now able to be formatted as HTML.

--- src/main/java/nl/getthere/services/PortalMailService.java
+++ src/main/java/nl/getthere/services/PortalMailService.java
@@ -1,8 +1,14 @@
 package nl.getthere.services;
 
+import java.io.File;
+
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,21 +17,22 @@ public class PortalMailService {
 	@Autowired
 	private MailSender mailSender;
 	
-	public void sendWelcomeMail(String email){
+	@Autowired
+	private JavaMailSender jMailSender;
 		
+	public void sendWelcomeMail(String email){
 		try{
-			
-			SimpleMailMessage msg = new SimpleMailMessage();
-			msg.setTo(email);
-			msg.setFrom("gtsptest@gmail.com");
-			msg.setSubject("Welkom op de Get There Studenten Portal!");
-			msg.setText("<h1>Welkom enzo.</h1> Groetjes.");
-			mailSender.send(msg);
-			System.out.println("Mail sent! Check "+email);
+			MimeMessage message = jMailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+			helper.setTo(email);
+			helper.setSubject("Welkom op de Get There Student Portal!");
+			helper.setText("<h3>Welkom!</h3><p>Leuk dat je je hebt aangemeld op de Get There Student Portal! Hier komt nog meer tekst...</p><p>Groeten, Get There</p><img src='cid:gt_logo'>", true);
+			FileSystemResource res = new FileSystemResource(new File("D:/projects/academy/GTStudentPortal/src/main/webapp/resources/img/logo_header.png"));
+			helper.addInline("gt_logo", res);
+			jMailSender.send(message);
 		}catch(Exception e){
 			System.out.println("Error while trying to send mail!");
 			e.printStackTrace();
 		}
-		
 	}
 }
